a2ba9c64c79d0de236f952b1394b9f1565324d0a
Ju-wanjune <wndhkswnssla@naver.com>
2019-01-23 12:39:29 +0900

    로그인 인터셉터
    한줄로 만든 코드, 주석 설명 추가

--- spring-test/src/main/java/kr/green/springtest/interceptor/LoginInterceptor.java
+++ spring-test/src/main/java/kr/green/springtest/interceptor/LoginInterceptor.java
@@ -13,7 +13,7 @@ import kr.green.springtest.vo.AccountVo;
 public class LoginInterceptor extends HandlerInterceptorAdapter{
 
 	@Override
-	public void postHandle(
+	public void postHandle(//컨트롤러로 가기 전 인터셉트
 	    HttpServletRequest request, 
 	    HttpServletResponse response, 
 	    Object handler, 
@@ -21,10 +21,11 @@ public class LoginInterceptor extends HandlerInterceptorAdapter{
 	    throws Exception {
 	    ModelMap modelMap = modelAndView.getModelMap();
 	    AccountVo user = (AccountVo)modelMap.get("user");
-
+	    //AccountVo user = (AccountVo)modelAndView.getModelMap().get("user");
 	    if(user != null) {
 	        HttpSession session = request.getSession();
 	        session.setAttribute("user", user);
+	        //request.getSession().setAttribute("user", user);
 	    }
 	}
 }
